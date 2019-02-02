10fc85f0a912ea16051221bf4168585058348b59
Michael Witrant <michael@ouvrages-web.fr>
2019-02-02 12:53:30 +0100

    Start HomePage

--- front/src/App.js
+++ front/src/App.js
@@ -1,31 +1,10 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import backUrl from './backUrl'
+import HomePage from './HomePage'
 
 class App extends Component {
-  onPing = () => {
-    fetch(backUrl("ping"))
-      .then(response => response.text())
-      .then(response => {
-        console.log(response)
-      })
-  }
-
   render() {
     return (
-      <div>
-        <AppBar position="static" color="default">
-          <Toolbar>
-            <Typography variant="h6" color="inherit">
-              ACoLock
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Button color="primary" onClick={this.onPing}>Ping</Button>
-      </div>
+      <HomePage />
     );
   }
 }
